4a3f9b819a1b1fe5115206c05b87b9b277f555ea
Shahzaib-shakir <108406250+Shahzaib-shakir@users.noreply.github.com>
2022-11-17 19:17:28 +0500

    my testingpurpose website

---
+++ dbconnection.php
@@ -0,0 +1,17 @@
+ <!-- locallay Database Connection -->
+ <?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', '');
+   define('DB_DATABASE', 'contact');
+   $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
+ 
+ <!-- Online Database Connection -->
+ <?php
+   // define('DB_SERVER', 'localhost');
+   // define('DB_USERNAME', 'id15722416_ffarleyhard');
+   // define('DB_PASSWORD', 'Lga^<2Z%U[tnL5m');
+   // define('DB_DATABASE', 'id15722416_human_resources');
+   // $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
